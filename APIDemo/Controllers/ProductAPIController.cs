a651501ae9d715ad46afcdc0aee88d5d038cbbd4
RevolutionAdmin <achyut.kendre@gmail.com>
2024-11-26 16:27:39 +0530

    Product API Controller

---
+++ APIDemo/Controllers/ProductAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
+    {
+    }
+}
